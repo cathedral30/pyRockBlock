853094bee9d723b43cfc0f8d133e1816460e8e61
cathedral30 <cooper.henry30@gmail.com>
2023-12-09 11:45:36 +0000

    project url

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+project_urls = {
+    'Documentation': 'https://cathedral30.github.io/pyRockBlock/'
+}
+
 setup(
     name='pyRockBlock',
-    version='1.0.3',
+    version='1.0.4',
     packages=['pyRockBlock'],
     url='https://github.com/cathedral30/pyRockBlock',
     license='',
@@ -10,4 +14,5 @@ setup(
     author_email='cooper.henry30@gmail.com',
     description='The definitive RockBLOCK Python 3 package.',
     long_description=open('README.rst').read(),
+    project_urls=project_urls
 )
